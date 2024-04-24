d1093d2dcdb06d99c505d21d617f9b703b5621da
Mehdi BENAISSA <mehdi.benaissa@getlinkgroup.com>
2024-04-24 17:14:55 +0200

    refactor: refactoring P2C2.2

--- P2/P2C2.2/Program.cs
+++ P2/P2C2.2/Program.cs
@@ -1,21 +1,21 @@
 ﻿using P2C2._2;
 
-Color selectedColor = Color.green;
+Color selectedColor = Color.Green;
 
 // TODO: Replace the if/else sequence with an equivalent switch statement that operates according to the value of the selectedColor variable
-if (selectedColor == Color.blue)
+if (selectedColor == Color.Blue)
 {
     Console.WriteLine("The color is blue");
 }
-else if (selectedColor == Color.green)
+else if (selectedColor == Color.Green)
 {
     Console.WriteLine("The color is green");
 }
-else if (selectedColor == Color.red)
+else if (selectedColor == Color.Red)
 {
     Console.WriteLine("The color is red");
 }
-else if (selectedColor == Color.yellow)
+else if (selectedColor == Color.Yellow)
 {
     Console.WriteLine("The color is yellow");
 }
